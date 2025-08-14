6238e044a748299103e2c52cf3e67f9194f82e0f
jun <pickjog@gmail.com>
2025-08-14 23:31:39 +0900

    update HttpRequest

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -52,4 +52,46 @@ public class HelloServlet extends HttpServlet {
     }
     /*요청정보를 운영서버에 남기면 성능저하가 날 수 있다.
     * */
+
+    /**
+     * HttpServletRequest역할
+     * HTTP요청 메세지를 개발자가 직접 파싱해서 사용해도 되지만, 매우 불편하다.
+     * 서블릿은 개발자가 HTTP 요청 메세지를 편리하게 사용할 수 있도록 개발자 대신 HTTP 요청 메세지를 파싱한다.
+     * 그리고 그 결과를 HttpServelitRequest객체에 담아서 제공한다.
+     *
+     * 서블릿은 이런 요청 메세지를 편리하게 요청 메세지를 파싱해주고
+     * 꺼내서 사용할 수 있도록 도와준다.
+     * 그리고 서블릿 객체로 담아서 사용
+     *
+     * Start Line
+     * - HTTP 메소드
+     * - URL
+     * - 쿼리스트링
+     * - 스키마,프로토콜
+     * -헤더
+     * - 헤더 조회
+     * - 바디
+     *  - form 파라미터 형식 조회
+     *  - message body 데이터 직접 조회
+     *
+     *  HttpServletRequest 객체는 추가로 여러가지 부가기능도 함께
+     *  제공한다.
+     *
+     *  *임시 저장소 기능*
+     *  해당 HTTP 요청이 시작부터 끝날때까지 유지되는 임시 저장소 기능
+     *  - 저장 : request.setAttribute(name,value)
+     *  - 조회 : request.getAttribute(name)
+     *  각 요청마다 요청메세지가 메모리에서 유지가 된다.
+     *  요청 생명주기에서
+     *
+     *  세션 관리
+     *  request.getSession(create: true)
+     *
+     *  중요!
+     *  HttpServletRequest/HttpServletResponse를 사용할 때 가장
+     *  중요한 점은 이 객체들이 HTTP요청 메세지,HTTP 응답 메세지를 편리하게
+     *  사용하도록 도와주는 객체라는 점이다. 따라서 이 기능에 대해서 깊이 있는 이해를 하렴녀
+     *  HTTP 스펙이 제공하는 요청,응답 자체를 이해를 해야된다.
+     *  
+     * */
 }
